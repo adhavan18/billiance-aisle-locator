2a1cc1159bedb57a1516f4c681e54dfadb8e1990
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2025-02-12 11:30:25 +0530

    Delete src directory

--- src/components/Animations/PageTransition.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './PageTransition.css';
-
-const PageTransition = ({ children }) => (
-  <div className="page-transition">{children}</div>
-);
-
-export default PageTransition;
-
